f531afabacdbc73b0e1880401537066dae4ec694
adrien <adrien.montfort@gmail.com>
2014-06-24 15:11:51 +0200

    Add .ideavimrc

--- create_links.sh
+++ create_links.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for f in .oh-my-zsh .vim .gitconfig .vimrc .zshrc .gvimrc; do
+for f in .oh-my-zsh .vim .gitconfig .vimrc .zshrc .gvimrc .ideavimrc; do
 	ln -sf adrien-config/$f ~
 done
 mkdir ~/.MacOSX
