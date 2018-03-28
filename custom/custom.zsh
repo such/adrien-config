3315a9bd681262384ec2811627781b614e05c309
Adrien Montfort <adrien@stratumn.com>
2018-03-28 16:06:07 +0200

    Move stratumn workspace

--- custom/custom.zsh
+++ custom/custom.zsh
@@ -8,10 +8,14 @@ bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
 bindkey "^?" backward-delete-char
 
-export STRATUMN_HOME=$HOME/workspace/stratumn
-export GOPATH=$HOME/workspace/go
+export STRATUMN_HOME=$HOME/workspace/src/github.com/stratumn
+export GOPATH=$HOME/workspace
 
 export PATH=$PATH:$GOPATH/bin
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/openssl/bin:$PATH"
+
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
 alias st='cd $STRATUMN_HOME'
 alias gost='cd $GOPATH/src/github.com/stratumn'
@@ -26,5 +30,7 @@ alias cd..="cd .."
 
 alias sz='source ~/.zshrc;cd ~/adrien-config/;git commit  ~/adrien-config/.zshrc -m "edit zshrc";git push origin master;cd -'
 alias ez='vim ~/.zshrc'
+
+alias awk=gawk
 
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=magenta'
